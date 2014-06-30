cdbfe0a281dd0a1dcb8edb264bd67471f4181fdf
Piotr Kazulak <pkazulak@pl.sii.eu>
2014-06-30 12:19:17 +0200

    BootstrapBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new EnterId\CmsBundle\EnterIdCmsBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\AopBundle\JMSAopBundle(),
+            new EnterId\BootstrapBundle\EnterIdBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/EnterId/BootstrapBundle/EnterIdBootstrapBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace EnterId\BootstrapBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EnterIdBootstrapBundle extends Bundle
+{
+}
